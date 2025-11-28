ec4f6c788311267c0b288e90985fd02fd0912e0a
copyrite <tatu.heikki.leinonen@gmail.com>
2025-11-28 23:08:12 +0200

    Remove commented out rules that probably won't be needed anyway

--- worlds/x2wotc/mods/lwotc/Rules.py
+++ worlds/x2wotc/mods/lwotc/Rules.py
@@ -8,24 +8,6 @@ from BaseClasses import CollectionState
 
 
 class RuleManager_LW(RuleManager):
-    # Facility Assault: Balancing omitted from first draft
-    # def can_do_facility_mission(self, state):
-
-    # Chosen hunt: Balancing omitted from first draft
-    # def has_resistance_ring(self, state):
-    # def can_meet_first_chosen(self, state):
-    # def can_meet_all_chosen(self, state):
-    # def can_hunt_all_chosen(self, state):
-    # def can_defeat_assassin(self, state):
-    # def can_defeat_hunter(self, state):
-    # def can_defeat_warlock(self, state):
-    # def can_defeat_one_chosen(self, state):
-    # def can_defeat_two_chosen(self, state):
-    # def can_defeat_all_chosen(self, state):
-    # def can_kill_assassin(self, state):
-    # def can_kill_hunter(self, state):
-    # def can_kill_warlock(self, state):
-
     def can_do_blacksite_mission(self, state):
         return self.can_make_more_contact(state)
 
