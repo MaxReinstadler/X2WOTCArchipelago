659e3bd161ccd6326c1f0993f170cb9b1bcd9753
MaxReinstadler <maxreinstadler10@gmail.com>
2025-12-14 02:46:25 +0100

    Bump version to 0.10.5 - update minimum compatible versions - require mod version 0.10.2 due to new option

--- worlds/x2wotc/Version.py
+++ worlds/x2wotc/Version.py
@@ -1,5 +1,5 @@
 # world_version is defined in archipelago.json manifest file
-minimum_client_version = "0.10.4"
-client_version = "0.10.4"
-minimum_world_version = "0.10.4"
-minimum_mod_version = "0.10.0"
+minimum_client_version = "0.10.5"
+client_version = "0.10.5"
+minimum_world_version = "0.10.5"
+minimum_mod_version = "0.10.2"
