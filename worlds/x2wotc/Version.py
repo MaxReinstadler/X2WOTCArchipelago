258d64ffb10617ab46b8cc63e42c3119d902a6c6
MaxReinstadler <maxreinstadler10@gmail.com>
2026-01-10 22:33:13 +0100

    Bump version to 0.10.8 - min_mod_version is now 0.10.3

--- worlds/x2wotc/Version.py
+++ worlds/x2wotc/Version.py
@@ -1,5 +1,5 @@
 # world_version is defined in archipelago.json manifest file
-minimum_client_version = "0.10.7"
-client_version = "0.10.7"
-minimum_world_version = "0.10.5"
-minimum_mod_version = "0.10.2"
+minimum_client_version = "0.10.8"
+client_version = "0.10.8"
+minimum_world_version = "0.10.8"
+minimum_mod_version = "0.10.3"
